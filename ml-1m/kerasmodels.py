95189812103f8ab97ef3e4c5f4f279db37521194
Parth Kashikar <kashikarparth@gmail.com>
2018-05-25 15:54:57 +0530

    worked on RMSE, Data Refeeding

--- ml-1m/kerasmodels.py
+++ ml-1m/kerasmodels.py
@@ -4,7 +4,7 @@ Created on Wed May 23 11:33:44 2018
 
 @author: Parth
 """
-
+# IMPORTS
 from keras.models import Sequential
 from keras.layers.core import Dense,Activation,Dropout
 from keras.optimizers import RMSprop
@@ -12,11 +12,12 @@ from keras.models import load_model
 import pandas as pd
 import numpy as np
 import scipy.sparse as sp
+############################################################################################
 
+#DATA PREPROCESSING
 data = pd.read_csv("ratings.dat", sep='::', header=None, skiprows=0, engine = 'python')
 group_data = data.groupby([0, 1]).sum().reset_index()
 group_data = group_data.drop(3, axis = 1)
-movies_lookup = pd.read_csv("movies.dat", sep = '::', header = None, skiprows = 0, engine = 'python')
 users = list(np.sort(group_data[0].unique()))
 products = list(np.sort(group_data[1].unique()))
 rating = list(group_data[2])
@@ -24,55 +25,67 @@ rows = group_data[0].astype('category',users).cat.codes
 columns = group_data[1].astype('category',products).cat.codes
 sparse = sp.csr_matrix((rating, (rows, columns)), shape = (len(users), len(products)))      
 #print(np.shape(sparse.toarray()))
+#############################################################################################
 
+#HELPER FUNCTIONS 
 def predict(x):
-    model2 = load_model('NvidiaDeepRec.h5')
-    return(np.round(model2.predict(x)))
-        
-if(0):
+    model2 = load_model('NvidiaDeepRec4.h5')
+    return(model2.predict(x))
+    
+def gen(x,y):                                           
+    sparse1 = []
+    for i in range(6039, -1, -1):
+        sparse1.append(np.round(y.predict(x[i].toarray())))
+        if(i%1000==0):
+            print("customers done: ", 6040-i)
+    sparse1 = np.reshape(sparse1,newshape=[-1,3706])
+    np.save('predicted_vals.npy',sparse1)
+#############################################################################################
+if(0):                                                      #MODEL TWEAK-TESTS
     model2 = Sequential()
-    model2.add(Dense(512, kernel_initializer='lecun_uniform', input_shape=(3706,)))
+    model2.add(Dense(128, kernel_initializer='lecun_uniform', input_shape=(3706,)))
     model2.add(Activation('selu'))
-    model2.add(Dense(512, kernel_initializer='lecun_uniform'))
+    model2.add(Dense(128, kernel_initializer='lecun_uniform'))
     model2.add(Activation('selu'))
-    model2.add(Dense(1024, kernel_initializer='lecun_uniform'))
+    model2.add(Dense(128, kernel_initializer='lecun_uniform'))
     model2.add(Activation('selu'))
     model2.add(Dropout(0.8))
-    model2.add(Dense(512, kernel_initializer='lecun_uniform'))
+    model2.add(Dense(128, kernel_initializer='lecun_uniform'))
     model2.add(Activation('selu'))
-    model2.add(Dense(512, kernel_initializer='lecun_uniform'))
+    model2.add(Dense(128, kernel_initializer='lecun_uniform'))
+    model2.add(Activation('selu'))
+    model2.add(Dense(128, kernel_initializer='lecun_uniform'))
     model2.add(Activation('selu'))
     model2.add(Dense(3706, kernel_initializer='lecun_uniform'))
     rms = RMSprop()
     model2.compile(loss='mmse', optimizer=rms)
-    model2.fit(x=sparse.toarray(),y=sparse.toarray(),batch_size=128,epochs = 20, verbose = 1) 
-    model2.save('NvidiaDeepRec.h5')
+    model2.fit(x=sparse.toarray(),y=sparse.toarray(),batch_size=128,epochs = 50, verbose = 1) 
+    model2.save('NvidiaDeepRec4.h5')
     
-if(0):
-    model2 = load_model('NvidiaDeepRec.h5')
-    model2.fit(x=sparse.toarray(),y=sparse.toarray(),batch_size=256,epochs = 10, verbose = 1) 
-    model2.save('NvidiaDeepRec.h5')
+if(0):                                                      #MODEL RETRAINER
+    model2 = load_model('NvidiaDeepRec4.h5')
+    model2.fit(x=sparse.toarray(),y=sparse.toarray(),batch_size=256,epochs = 50, verbose = 1) 
+    model2.save('NvidiaDeepRec4.h5')
 
-if(0):                                            #DATA REFEEDING
-    model2 = load_model('NvidiaDeepRec.h5')
-    for i in range(6040):
-        x1 = predict(sparse[i].toarray())
-        model2.fit(x=x1,y=x1,batch_size=128,epochs = 1, verbose = 1)
-    model2.save('NvidiaDeepRec.h5')
+if(0):
+    model2 = load_model('NvidiaDeepRec4.h5')
+    gen(sparse, model2)                                                    #DATA REFEEDING
+    sparse1 = np.load("predicted_vals.npy")
+    model2.fit(x=sparse1,y=sparse1,batch_size=256,epochs = 30, verbose = 1) 
+    model2.save('NvidiaDeepRec4.h5')
     
-if(0):    
+if(0):                                                      #OUTPUT CHECKER
     print(sparse[8].toarray())
     print(predict(sparse[8].toarray()))
 
-if(0):
-    sum = 0
-    ssum = 0
-    for i in range(10):
-        temp = predict(sparse[i].toarray())[0]
-        for j in range(3706):
-            if(sparse[i].toarray()[0][j]!=0):
-                sum+=1
-                ssum = (temp[j]-sparse[i].toarray()[0][j])**2
-    print((ssum/sum)**0.5)
-            
-            
+if(1):                                                      #RMSE CALCULATOR
+    model2 = load_model('NvidiaDeepRec4.h5')
+    gen(sparse, model2)
+    sparse1 = np.load("predicted_vals.npy")
+    sparse2 = sparse.toarray()
+    num = np.count_nonzero(sparse2)
+    sparsetemp = np.multiply(sparse2,sparse1)
+    sparse1 = np.divide(sparsetemp,sparse2, where=sparse2!=0)
+    sparsefinal = np.sum(np.square(np.subtract(sparse2,sparse1)))*(1/num)
+    print(np.sqrt(sparsefinal))
+##############################################################################################
